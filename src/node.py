25e74752ecc880d8be94564033eca5281b36fef3
Jennifer Lam <jl87@princeton.edu>
2023-03-04 16:54:56 -0500

    add __hash__() to Node class

--- src/node.py
+++ src/node.py
@@ -1,10 +1,11 @@
 class Node:
-
     """ Represents information about a node."""
 
     def __init__(self, ip_enum):
-
         self.ip = "192.168.1." + str(ip_enum)
 
     def __str__(self):
         return str(vars(self))
+
+    def __hash__(self):
+        return hash(self.ip)
