3d0781f65b2baa592d225f0fc5f82e25fd822e1d
Jennifer Lam <jl87@princeton.edu>
2023-03-04 17:07:01 -0500

    debug

--- src/main.py
+++ src/main.py
@@ -47,6 +47,10 @@ def main():
         db_dir, "configs_to_process.csv"
     )
 
+    # create the database and table
+    if not os.path.exists(db_dir):
+        os.makedirs(db_dir)
+
     # populate configs to process
     for cfg_obj in CONFIG_OBJ_LIST:
         cfg_fpath_list = cfg_obj.generate_all_config_files()
@@ -55,52 +59,52 @@ def main():
         } for cfg_fpath in cfg_fpath_list]
         csv_utils.append_data_to_file(data, files_to_process)
 
-        # file of failed configs
-        failed_configs_csv = os.path.join(db_dir, "failed_configs.csv")
-        f = open(
-            failed_configs_csv, "w"
-        )  # make sure it's only the failures from this round
-        f.close()
+    # file of failed configs
+    failed_configs_csv = os.path.join(db_dir, "failed_configs.csv")
+    f = open(
+        failed_configs_csv, "w"
+    )  # make sure it's only the failures from this round
+    f.close()
 
-        # connect to db
-        db = sqlite_helper_object.SQLiteHelperObject(
-            os.path.join(db_dir, "trials.db")
+    # connect to db
+    db = sqlite_helper_object.SQLiteHelperObject(
+        os.path.join(db_dir, "trials.db")
+    )
+    db.connect()
+    _, cfg_lt_tuples = csv_utils.read_in_data_as_tuples(
+        files_to_process, has_header=False
+    )
+
+    for cfg_fpath, lt_fpath in cfg_lt_tuples:
+        # generate config object
+        cfg = generate_configs.generate_configs_from_files_and_add_fields(
+            cfg_fpath
         )
-        db.connect()
-        _, cfg_lt_tuples = csv_utils.read_in_data_as_tuples(
-            files_to_process, has_header=False
+
+        # generate lt_config objects that match those config objects
+        lt_cfg = config_io.read_config_from_file(lt_fpath)
+
+        # make directory in which trial will be run
+        logs_dir = generate_dir_name(
+            db_dir,
+            read_percent=cfg["read_percent"],
+            client_concurrency=cfg["client_concurrency"],
+            server_concurrency=cfg["server_concurrency"],
         )
 
-        for cfg_fpath, lt_fpath in cfg_lt_tuples:
-            # generate config object
-            cfg = generate_configs.generate_configs_from_files_and_add_fields(
-                cfg_fpath
+        if not os.path.exists(logs_dir):
+            os.makedirs(logs_dir)
+
+        # copy over config into directory
+        system_utils.call(
+            "cp {0} {1}".format(
+                cfg[constants.CONFIG_FPATH_KEY], logs_dir
             )
+        )
 
-            # generate lt_config objects that match those config objects
-            lt_cfg = config_io.read_config_from_file(lt_fpath)
-
-            # make directory in which trial will be run
-            logs_dir = generate_dir_name(
-                db_dir,
-                read_percent=cfg["read_percent"],
-                client_concurrency=cfg["client_concurrency"],
-                server_concurrency=cfg["server_concurrency"],
-            )
-
-            if not os.path.exists(logs_dir):
-                os.makedirs(logs_dir)
-
-            # copy over config into directory
-            system_utils.call(
-                "cp {0} {1}".format(
-                    cfg[constants.CONFIG_FPATH_KEY], logs_dir
-                )
-            )
-
-            # clone smdbrpc repo on all hosts
-            for n in cfg["clients"]:
-                clone(cfg["client_branch"], n.ip)
+        # clone smdbrpc repo on all hosts
+        for n in cfg["clients"]:
+            clone(cfg["client_branch"], n.ip)
 
     return 0
 
